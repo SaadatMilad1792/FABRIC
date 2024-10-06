d68a824f8d60b5f4c9c9da6b29dd0428cf9fbbf3
Milad Saadat <saadatmilad1792@gmail.com>
2024-10-05 19:49:40 -0500

    Fixed input handling

--- FABRIC/FILE/bsObject.py
+++ FABRIC/FILE/bsObject.py
@@ -1,8 +1,8 @@
 #######################################################################################################################
 ## -- necessary libraries -- ##########################################################################################
 #######################################################################################################################
-import numpy as np
-from struct import unpack
+import sys
+from ..FILE import *
 
 #######################################################################################################################
 ## -- binary stream object generator -- ###############################################################################
@@ -12,3 +12,16 @@ def bsObject(params):
   subParamObject = params["process"]
   inpDirectory, inpFolder = subParamObject["inpDirectory"], subParamObject["inpFolder"]
   outDirectory, outFolder = subParamObject["outDirectory"], subParamObject["outFolder"]
+  experimentTypes = subParamObject["experimentTypes"]
+    
+  expType = dirSweep(inpDirectory + inpFolder)
+  if not set(expType).issubset(set(experimentTypes)):
+    sys.exit(f"Unknown Experiment Type Detected, Process Terminated. Allowed Types: {experimentTypes}")
+
+  if os.path.isdir(outDirectory):
+    outFullPath = os.path.join(outDirectory, outFolder)
+    if not os.path.exists(outFullPath):
+      os.makedirs(outFullPath)
+  else:
+    sys.exit(f"Directory '{outDirectory}' is invalid. Add a valid directory in 'params -> process -> outDirectory'")
+    
