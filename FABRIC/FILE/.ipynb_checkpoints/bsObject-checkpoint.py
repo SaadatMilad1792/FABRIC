7d8c553169bbe0581f71cbc6fe81f9daf5e1cf06
Milad Saadat <saadatmilad1792@gmail.com>
2024-10-08 14:44:51 -0500

    Thread based parallelism to CPU based

--- FABRIC/FILE/.ipynb_checkpoints/bsObject-checkpoint.py
+++ FABRIC/FILE/.ipynb_checkpoints/bsObject-checkpoint.py
@@ -15,54 +15,111 @@ from FABRIC.SUPPORT.code_descriptors_postural_control.descriptors import compute
 ## -- binary stream object generator -- ###############################################################################
 #######################################################################################################################
 def bsObject(params, directory, fileName):
-  
   subParamObject = params["process"]
   featExtr = subParamObject["featExtr"]
   framePerSecond, verbose = subParamObject["framePerSecond"], subParamObject["verbose"]
-  
+
   codeName = fileName.split(".")[0].split("_")
   subId, expType, expStage, expCond = codeName[0], codeName[1], codeName[2], list(codeName[3])
   expName, stabilityStatus, eyeStatus, trial = expCond[0], expCond[1], expCond[2], expCond[3]
-  data, _, _ = loadbsf(os.path.join(directory, fileName), plot = False)
   
-  dataPacket = []
-  for i in range(len(data)):
-    if (i + 1) % 1000 == 0 and verbose:
-      print(f"Iter {(i + 1)}, thread: {threading.get_ident()}")
-    
-    dataPacket.append({
-      "subId": subId, 
-      "expType": expType, 
-      "expStage": expStage, 
-      "time (s)": (i + 1) / framePerSecond,
-      "expTrial": trial, 
-      "expName": "Balance" if expName == "B" else "Unknown",  
-      "stabilityStatus": "Stable" if stabilityStatus == "S" else "Unstable" if stabilityStatus == "U" else "Unknown",
-      "eyeStatus": "Closed" if eyeStatus == "C" else "Open" if eyeStatus == "O" else "Unknown",
-      "Fx": data[i][0], "Fy": data[i][1], "Fz": data[i][2], 
-      "Mx": data[i][3], "My": data[i][4], "Mz": data[i][5],
-    })
+  data, _, _ = loadbsf(os.path.join(directory, fileName), plot=False)
+  
+  dataPacket = pd.DataFrame([{
+    "subId": subId,
+    "expType": expType,
+    "expStage": expStage,
+    "expTrial": trial,
+    "expName": "Balance" if expName == "B" else "Unknown",
+    "stabilityStatus": "Stable" if stabilityStatus == "S" else "Unstable" if stabilityStatus == "U" else "Unknown",
+    "eyeStatus": "Closed" if eyeStatus == "C" else "Open" if eyeStatus == "O" else "Unknown",
+    "time (s)": (i + 1) / framePerSecond,
+    "Fx": data[i][0], "Fy": data[i][1], "Fz": data[i][2],
+    "Mx": data[i][3], "My": data[i][4], "Mz": data[i][5],
+  } for i in range(len(data))])
 
-  dataPacket = pd.DataFrame(dataPacket).reset_index(drop = True)
+  COPx = 100 * ((dataPacket["My"] / dataPacket["Fz"]) - np.mean(dataPacket["My"] / dataPacket["Fz"]))
+  COPy = 100 * ((dataPacket["Mx"] / dataPacket["Fz"]) - np.mean(dataPacket["Mx"] / dataPacket["Fz"]))
+
+  dataPacket["COPx"], dataPacket["COPy"] = COPx, COPy
+
+  featurePacket = {
+    "subId": subId,
+    "expType": expType,
+    "expStage": expStage,
+    "expTrial": trial,
+    "expName": "Balance" if expName == "B" else "Unknown",
+    "stabilityStatus": "Stable" if stabilityStatus == "S" else "Unstable" if stabilityStatus == "U" else "Unknown",
+    "eyeStatus": "Closed" if eyeStatus == "C" else "Open" if eyeStatus == "O" else "Unknown",
+    "dataPacket": dataPacket
+  }
+
   if featExtr:
-    
     try:
-    
-      COPx = 100 * ((dataPacket["My"] / dataPacket["Fz"]) - np.mean(dataPacket["My"] / dataPacket["Fz"]))
-      COPy = 100 * ((dataPacket["Mx"] / dataPacket["Fz"]) - np.mean(dataPacket["Mx"] / dataPacket["Fz"]))
-      COP, dataPacket["COPx"], dataPacket["COPy"] = np.array([np.array(COPx), np.array(COPy)]).T, COPx, COPy
-
       stato = Stabilogram()
-      stato.from_array(array = COP, original_frequency = framePerSecond)
-      features = compute_all_features(stato, params_dic = {"sway_density_radius": 0.3})
+      stato.from_array(array=np.column_stack((COPx, COPy)), original_frequency=framePerSecond)
+      features = compute_all_features(stato, params_dic={"sway_density_radius": 0.3})
+
       for key, value in features.items():
-        try:
-          dataPacket[key] = value
-        except:
-          dataPacket[key] = False
-        
-    except:
+        featurePacket[key] = value
+
+    except Exception as e:
       print(f"[-] Failed to extract features due to invalid values: {fileName}")
+      print(str(e))
       sys.exit(f"[-] Failed to extract features due to invalid values: {fileName}")
+
+  return pd.DataFrame([featurePacket])
+
+  
+# def bsObject(params, directory, fileName):
+  
+#   subParamObject = params["process"]
+#   featExtr = subParamObject["featExtr"]
+#   framePerSecond, verbose = subParamObject["framePerSecond"], subParamObject["verbose"]
+  
+#   codeName = fileName.split(".")[0].split("_")
+#   subId, expType, expStage, expCond = codeName[0], codeName[1], codeName[2], list(codeName[3])
+#   expName, stabilityStatus, eyeStatus, trial = expCond[0], expCond[1], expCond[2], expCond[3]
+#   data, _, _ = loadbsf(os.path.join(directory, fileName), plot = False)
+  
+#   dataPacket = []
+#   for i in range(len(data)):
+#     if (i + 1) % 1000 == 0 and verbose:
+#       print(f"Iter {(i + 1)}, thread: {threading.get_ident()}")
     
-  return dataPacket
+#     dataPacket.append({
+#       "subId": subId, 
+#       "expType": expType, 
+#       "expStage": expStage, 
+#       "time (s)": (i + 1) / framePerSecond,
+#       "expTrial": trial, 
+#       "expName": "Balance" if expName == "B" else "Unknown",  
+#       "stabilityStatus": "Stable" if stabilityStatus == "S" else "Unstable" if stabilityStatus == "U" else "Unknown",
+#       "eyeStatus": "Closed" if eyeStatus == "C" else "Open" if eyeStatus == "O" else "Unknown",
+#       "Fx": data[i][0], "Fy": data[i][1], "Fz": data[i][2], 
+#       "Mx": data[i][3], "My": data[i][4], "Mz": data[i][5],
+#     })
+
+#   dataPacket = pd.DataFrame(dataPacket).reset_index(drop = True)
+#   if featExtr:
+    
+#     try:
+    
+#       COPx = 100 * ((dataPacket["My"] / dataPacket["Fz"]) - np.mean(dataPacket["My"] / dataPacket["Fz"]))
+#       COPy = 100 * ((dataPacket["Mx"] / dataPacket["Fz"]) - np.mean(dataPacket["Mx"] / dataPacket["Fz"]))
+#       COP, dataPacket["COPx"], dataPacket["COPy"] = np.array([np.array(COPx), np.array(COPy)]).T, COPx, COPy
+
+#       stato = Stabilogram()
+#       stato.from_array(array = COP, original_frequency = framePerSecond)
+#       features = compute_all_features(stato, params_dic = {"sway_density_radius": 0.3})
+#       for key, value in features.items():
+#         try:
+#           dataPacket[key] = value
+#         except:
+#           dataPacket[key] = False
+        
+#     except:
+#       print(f"[-] Failed to extract features due to invalid values: {fileName}")
+#       sys.exit(f"[-] Failed to extract features due to invalid values: {fileName}")
+    
+#   return dataPacket
--- FABRIC/FILE/.ipynb_checkpoints/genFabDf-checkpoint.py
+++ FABRIC/FILE/.ipynb_checkpoints/genFabDf-checkpoint.py
@@ -1,95 +1,3 @@
-# #######################################################################################################################
-# ## -- necessary libraries -- ##########################################################################################
-# #######################################################################################################################
-# import os
-# import sys
-# import threading
-# from ..FILE import *
-# from concurrent.futures import ThreadPoolExecutor
-
-# #######################################################################################################################
-# ## -- binary stream object generator -- ###############################################################################
-# #######################################################################################################################
-# def genFabDf(params):
-  
-#   subParamObject = params["process"]
-#   debuggingMode, featExtr = subParamObject["debuggingMode"], subParamObject["featExtr"]
-#   outPickleName, funcStat = subParamObject["outPickleName"], subParamObject["funcStat"]
-#   parallelProc, maxWorker = subParamObject["parallelProc"], subParamObject["maxWorker"]
-#   inpDirectory, inpFolder = subParamObject["inpDirectory"], subParamObject["inpFolder"]
-#   outDirectory, outFolder = subParamObject["outDirectory"], subParamObject["outFolder"]
-#   experimentTypes, dfType = subParamObject["experimentTypes"], subParamObject["dfType"]
-  
-#   if not funcStat:
-#     print(f"[+] FABRIC: genFabDf marked as deactive, modify param.yaml for activation.")
-#     return
-    
-#   expTypes = dirSweep(os.path.join(inpDirectory, inpFolder))
-#   if not set(expTypes).issubset(set(experimentTypes)):
-#     print(f"[-] Unknown Experiment Type Detected, Process Terminated. Allowed Types: {experimentTypes}")
-#     sys.exit(f"[-] Unknown Experiment Type Detected, Process Terminated. Allowed Types: {experimentTypes}")
-#   elif debuggingMode:
-#     expTypes = expTypes[:1]
-
-#   if os.path.isdir(outDirectory):
-#     outFullPath = os.path.join(outDirectory, outFolder)
-#     if not os.path.exists(outFullPath):
-#       os.makedirs(outFullPath)
-#   else:
-#     print(f"[-] Directory '{outDirectory}' is invalid. Add a valid directory in 'params > process > outDirectory'")
-#     sys.exit(f"[-] Directory '{outDirectory}' is invalid. Add a valid directory in 'params > process > outDirectory'")
-    
-#   dataFrame = []
-#   if not parallelProc:
-#     for expType in expTypes:
-#       dataFiles = dirSweep(os.path.join(inpDirectory, inpFolder, expType))
-#       dataFiles = [f for f in dataFiles if f.split(".")[1] == dfType]
-      
-#       if debuggingMode:
-#         dataFiles = dataFiles[:1]
-    
-#       for fc, dataFile in enumerate(dataFiles):
-#         print(f"{dataFiles[fc]}".ljust(24), f" | Progress: ".ljust(12),
-#               f"[{(fc + 1):04} / {len(dataFiles):04}] -> ({(100 * (fc + 1) / len(dataFiles)):.3f} %)".ljust(16))
-#         dataFrame.append(bsObject(params, os.path.join(inpDirectory, inpFolder, expType), dataFile))
-   
-#   elif parallelProc:
-#     def bsObjectCompact(expType, dataFile):
-#       result = bsObject(params, os.path.join(inpDirectory, inpFolder, expType), dataFile)
-#       print(f"{dataFile}".ljust(24), "-> [STATUS: DONE]".ljust(16), flush = True)
-#       return result
-
-#     allResults = []
-#     with ThreadPoolExecutor(max_workers = maxWorker) as executor:
-#       futures = []
-#       for expType in expTypes:
-#         dataFiles = dirSweep(os.path.join(inpDirectory, inpFolder, expType))
-#         dataFiles = [f for f in dataFiles if f.split(".")[1] == dfType]
-        
-#         if debuggingMode:
-#           dataFiles = dataFiles[:1]
-
-#         for fc, dataFile in enumerate(dataFiles):
-#           futures.append(executor.submit(bsObjectCompact, expType, dataFile))
-#           print(f"{dataFiles[fc]}".ljust(24), f" | Progress: ".ljust(12),
-#                 f"[{(fc + 1):04} / {len(dataFiles):04}] -> ({(100 * (fc + 1) / len(dataFiles)):.3f} %)".ljust(16))
-
-#       for future in futures:
-#         result = future.result()
-#         allResults.append(result)
-
-#     dataFrame.extend(allResults)
-  
-#   else:
-#     print(f"[-] Invalid parallel type. Valid choices: ['True', 'False']")
-#     sys.exit(f"[-] Invalid parallel type. Valid choices: ['True', 'False']")
-  
-#   print(f"FABRIC [STATUS: DONE] -> Generated f'{outPickleName}.pkl.gz")
-#   dataFrame = pd.concat(dataFrame).reset_index(drop = True)
-#   dataFrame.to_pickle(os.path.join(outDirectory, outFolder, f'{outPickleName}.pkl.gz'), compression = 'gzip')
-#   return dataFrame
-
-
 #######################################################################################################################
 ## -- necessary libraries -- ##########################################################################################
 #######################################################################################################################
@@ -105,8 +13,11 @@ from concurrent.futures import ProcessPoolExecutor
 def bsObjectCompact(params, expType, dataFile):
   subParamObject = params["process"]
   inpDirectory, inpFolder = subParamObject["inpDirectory"], subParamObject["inpFolder"]
-  result = bsObject(params, os.path.join(inpDirectory, inpFolder, expType), dataFile)
-  print(f"{dataFile}".ljust(24), "-> [STATUS: DONE]".ljust(16), flush = True)
+  try:
+    result = bsObject(params, os.path.join(inpDirectory, inpFolder, expType), dataFile)
+    print(f"{dataFile}".ljust(24), "-> [STATUS: DONE]".ljust(16), flush = True)
+  except:
+    return
   return result
 
 #######################################################################################################################
@@ -157,7 +68,7 @@ def genFabDf(params):
    
   elif parallelProc:
     allResults = []
-    with ProcessPoolExecutor(max_workers=maxWorker) as executor:
+    with ProcessPoolExecutor(max_workers = maxWorker) as executor:
       futures = []
       for expType in expTypes:
         dataFiles = dirSweep(os.path.join(inpDirectory, inpFolder, expType))
